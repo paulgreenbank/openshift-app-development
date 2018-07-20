b424cd955e6380ed77e1f33d1f691ba33aacbfdb
paul greenbank <pgreenbank-oss.co.nz@bastion.6084.example.opentlc.com>
2018-07-20 11:04:31 +0000

    Updated Nexus Build Artifacts

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Setup Nexus Project
+
 if [ "$#" -ne 1 ]; then
     echo "Usage:"
     echo "  $0 GUID"
@@ -9,23 +10,37 @@ fi
 GUID=$1
 echo "Setting up Nexus in project $GUID-nexus"
 
-# Code to set up the Nexus. It will need to
-# * Create Nexus
-# * Set the right options for the Nexus Deployment Config
-# * Load Nexus with the right repos
-# * Configure Nexus as a docker registry
-# Hint: Make sure to wait until Nexus if fully up and running
-#       before configuring nexus with repositories.
-#       You could use the following code:
-# while : ; do
-#   echo "Checking if Nexus is Ready..."
-#   oc get pod -n ${GUID}-nexus|grep '\-2\-'|grep -v deploy|grep "1/1"
-#   [[ "$?" == "1" ]] || break
-#   echo "...no. Sleeping 10 seconds."
-#   sleep 10
-# done
+# Change to Nexus Project
+oc project ${GUID}-nexus
 
-# Ideally just calls a template
-# oc new-app -f ../templates/nexus.yaml --param .....
+# Setup Nexus ImageStream for build
+oc import-image nexus3 --from=sonatype/nexus3 --confirm
 
-# To be Implemented by Student
+# Create PersistentVolumeClaim for Nexus data
+echo "apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: nexus-pvc
+spec:
+  accessModes:
+  - ReadWriteOnce
+  resources:
+    requests:
+      storage: 4Gi" | oc create -f -
+
+# Process template and create environment
+sed "s/GUID/${GUID}/g" ../templates/nexus_template_build.yaml | oc process -f - | oc create -f -
+
+# Wait for Nexus to fully deploy and become ready
+while : ; do
+  echo "Checking if Nexus is Ready..."
+  #oc get pod -n ${GUID}-nexus | grep -v deploy | grep "1/1"
+  curl -i http://$(oc get route nexus3 --template='{{ .spec.host }}') 2>&1 /dev/null | grep 'HTTP/1.1 200 OK' > /dev/null
+  [[ "$?" == "1" ]] || break
+  echo "... not quite yet. Sleeping 20 seconds."
+  sleep 20
+done
+
+# Setup Nexus Repositoies via script sourced from
+# https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
+./configure_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}')
