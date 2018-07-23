8eca07903a85d78d15df73f79ed7a3592328d5b8
paul greenbank <pgreenbank-oss.co.nz@bastion.6084.example.opentlc.com>
2018-07-23 10:18:32 +0000

    Updated Jenkins build script

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -12,18 +12,22 @@ REPO=$2
 CLUSTER=$3
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
-# Code to set up the Jenkins project to execute the
-# three pipelines.
-# This will need to also build the custom Maven Slave Pod
-# Image to be used in the pipelines.
-# Finally the script needs to create three OpenShift Build
-# Configurations in the Jenkins Project to build the
-# three micro services. Expected name of the build configs:
-# * mlbparks-pipeline
-# * nationalparks-pipeline
-# * parksmap-pipeline
-# The build configurations need to have two environment variables to be passed to the Pipeline:
-# * GUID: the GUID used in all the projects
-# * CLUSTER: the base url of the cluster used (e.g. na39.openshift.opentlc.com)
+# Change to Nexus Project
+oc project ${GUID}-jenkins
 
-# To be Implemented by Student
+# Create master Jenkins build
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi
+
+# Setup Jenkins Mavin ImageStream for Jenkins slave builds
+oc new-build --name=jenkins-slave-maven-skopeo-centos7 -D $'FROM openshift/jenkins-slave-maven-centos7:v3.9\nUSER root\nRUN yum -y install skopeo apb && yum clean all\nUSER 1001'
+
+# Tag Jenkins slave ImageStream to version 3.9
+oc tag jenkins-slave-maven-skopeo-centos7:latest jenkins-slave-maven-skopeo-centos7:v3.9
+
+# Label Jenkins slave ImageStream for Jenkins to use for slave builds
+oc label imagestream jenkins-slave-maven-skopeo-centos7 role=jenkins-slave
+
+# Create the three pipeline build configs
+sed 's/GUID_VARIABLE/oss/g;s/CLUSTER_VARIABLE/mycluster.com/g' ../templates/mlbparks-pipeline_template_build.yaml | oc process -f - | oc create -f -
+sed 's/GUID_VARIABLE/oss/g;s/CLUSTER_VARIABLE/mycluster.com/g' ../templates/nationalparks-pipeline_template_build.yaml | oc process -f - | oc create -f -
+sed 's/GUID_VARIABLE/oss/g;s/CLUSTER_VARIABLE/mycluster.com/g' ../templates/parksmap-pipeline_template_build.yaml | oc process -f - | oc create -f -
