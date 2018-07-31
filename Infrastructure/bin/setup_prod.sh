02163b8ab8308de9e8e17c2b6610ff68655bb24a
paul greenbank <pgreenbank-oss.co.nz@bastion.4a0b.example.opentlc.com>
2018-07-31 09:50:49 +0000

    Updated Prod Infrastructure setup script

--- Infrastructure/bin/setup_prod.sh
+++ Infrastructure/bin/setup_prod.sh
@@ -13,9 +13,88 @@ echo "Setting up Parks Production Environment in project ${GUID}-parks-prod"
 oc project ${GUID}-parks-prod
 
 # Create MongoDB statefulset servers with 3 replicas from template
-oc process -f ../templates/mongodb_statefulset_template_build.yaml | oc create -f -
+#-oc process -f ../templates/mongodb_statefulset_template_build.yaml | oc create -f -
 
-# Code to set up the parks production project. It will need a StatefulSet MongoDB, and two applications each (Blue/Green) for NationalParks, MLBParks and Parksmap.
-# The Green services/routes need to be active initially to guarantee a successful grading pipeline run.
+#-oc policy add-role-to-group system:image-puller system:serviceaccounts:${GUID}-parks-prod -n ${GUID}-parks-dev
+#-oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-parks-prod
 
-# To be Implemented by Student
+# Create shared configmap for database connection
+#-oc create configmap mongodb-config --from-literal=DB_HOST=mongodb --from-literal=DB_PORT=27017 --from-literal=DB_USERNAME=mongodb --from-literal=DB_PASSWORD=mongodb --from-literal=DB_NAME=parks --from-literal=DB_REPLICASET=rs0
+
+## Tasks for MLBParks Blue microservice setup ##
+# Create binary build using jboss-eap container called mlbparks-blue
+#-oc new-build --binary=true --name="mlbparks-blue" jboss-eap70-openshift:1.7 -n ${GUID}-parks-prod
+# Add image stream using mlbparks-blue binary build
+#-oc new-app mlbparks-blue --name=mlbparks-blue --allow-missing-imagestream-tags=true -n ${GUID}-parks-prod
+# Remove build triggers from mlbparks-blue deployment config
+#-oc set triggers dc/mlbparks-blue --remove-all -n ${GUID}-parks-prod
+# Create configmap which will be loaded as env variables
+#-oc create configmap mlbparks-config --from-literal=APPNAME="MLB Parks (Blue)"
+# Add env variables from configmap to mlbparks-blue deployment config
+#-oc set env --from=configmap/mongodb-config dc/mlbparks-blue
+#-oc set env --from=configmap/mlbparks-config dc/mlbparks-blue
+# Expose mlbparks-blue service on port 8080 for deployment testing
+#-oc expose dc mlbparks-blue --port 8080 --labels=activeApp=mlbparks-blue -n ${GUID}-parks-prod
+
+## Tasks for MLBParks Green microservice setup ##
+# Create binary build using jboss-eap container called mlbparks-green
+#-oc new-build --binary=true --name="mlbparks-green" jboss-eap70-openshift:1.7 -n ${GUID}-parks-prod
+# Add image stream using mlbparks-green binary build
+#-oc new-app mlbparks-green --name=mlbparks-green --allow-missing-imagestream-tags=true -n ${GUID}-parks-prod
+# Remove build triggers from mlbparks-green deployment config
+#-oc set triggers dc/mlbparks-green --remove-all -n ${GUID}-parks-prod
+# Add env variables from configmap to mlbparks-green deployment config
+#-oc set env --from=configmap/mongodb-config dc/mlbparks-green
+#-oc set env --from=configmap/mlbparks-config dc/mlbparks-green
+# Expose mlbparks-green service on port 8080 for deployment testing
+#-oc expose dc mlbparks-green --port 8080 --labels=activeApp=mlbparks-green -n ${GUID}-parks-prod
+
+## Tasks for NationalParks Blue microservice setup ##
+# Create binary build using redhat-openjdk18 container called nationalparks-blue
+#-oc new-build --binary=true --name="nationalparks-blue" redhat-openjdk18-openshift:1.2 -n ${GUID}-parks-prod
+# Add image stream using nationalparks-blue binary build
+#-oc new-app nationalparks-blue --name=nationalparks-blue --allow-missing-imagestream-tags=true -n ${GUID}-parks-prod
+# Remove build triggers from nationalparks-blue deployment config
+#-oc set triggers dc/nationalparks-blue --remove-all -n ${GUID}-parks-prod
+# Create configmap which will be loaded as env variables
+#-oc create configmap nationalparks-config --from-literal=APPNAME="MLB Parks (Blue)"
+# Add env variables from configmap to nationalparks-blue deployment config
+#-oc set env --from=configmap/mongodb-config dc/nationalparks-blue
+#-oc set env --from=configmap/nationalparks-config dc/nationalparks-blue
+# Expose nationalparks-blue service on port 8080 for deployment testing
+#-oc expose dc nationalparks-blue --port 8080 --labels=activeApp=nationalparks-blue -n ${GUID}-parks-prod
+
+## Tasks for NationalParks Green microservice setup ##
+# Create binary build using redhat-openjdk18 container called nationalparks-green
+#-oc new-build --binary=true --name="nationalparks-green" redhat-openjdk18-openshift:1.2 -n ${GUID}-parks-prod
+# Add image stream using nationalparks-green binary build
+#-oc new-app nationalparks-green --name=nationalparks-green --allow-missing-imagestream-tags=true -n ${GUID}-parks-prod
+# Remove build triggers from nationalparks-green deployment config
+#-oc set triggers dc/nationalparks-green --remove-all -n ${GUID}-parks-prod
+# Add env variables from configmap to nationalparks-green deployment config
+#-oc set env --from=configmap/mongodb-config dc/nationalparks-green
+#-oc set env --from=configmap/nationalparks-config dc/nationalparks-green
+# Expose nationalparks-green service on port 8080 for deployment testing
+#-oc expose dc nationalparks-green --port 8080 --labels=activeApp=nationalparks-green -n ${GUID}-parks-prod
+
+##  Tasks for ParksMap microservice setup ##
+# Allow permissions for discovering backend services
+oc policy add-role-to-user view --serviceaccount=default
+# Create binary build using openjdk18 container called parksmap-blue
+oc new-build --binary=true --name="parksmap-blue" redhat-openjdk18-openshift:1.2 -n ${GUID}-parks-prod
+# Add image stream using parksmap-blue binary build
+oc new-app parksmap-blue --name=parksmap-blue --allow-missing-imagestream-tags=true -n ${GUID}-parks-prod
+# Remoe build triggers from parksmap deployment config
+oc set triggers dc/parksmap-blue --remove-all -n ${GUID}-parks-prod
+# Expose parksmap-blue service on port 8080
+oc expose dc parksmap-blue --port 8080 --labels=type=parksmap-blue -n ${GUID}-parks-prod
+
+##  Tasks for ParksMap microservice setup ##
+# Create binary build using openjdk18 container called parksmap-green
+oc new-build --binary=true --name="parksmap-green" redhat-openjdk18-openshift:1.2 -n ${GUID}-parks-prod
+# Add image stream using parksmap-green binary build
+oc new-app parksmap-green --name=parksmap-green --allow-missing-imagestream-tags=true -n ${GUID}-parks-prod
+# Remove build triggers from parksmap deployment config
+oc set triggers dc/parksmap-green --remove-all -n ${GUID}-parks-prod
+# Expose parksmap-green service on port 8080
+oc expose dc parksmap-green --port 8080 --labels=type=parksmap-green -n ${GUID}-parks-prod
